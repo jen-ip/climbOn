0aac761cc589c004c5396993051aacc470852907
Jennifer Ip <sai@JenniferIpsMBP.home>
2016-08-22 20:32:13 -0400

    reordered menu, and got th the filter button working kinda, but now other problems have risen and still havent figured out the whole lightbox problem..

--- assets/js/opening.js
+++ assets/js/opening.js
@@ -1,21 +1,48 @@
-var d = new Date();
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("dayOne").innerHTML = days[d.getDay()];
+// var d = new Date();
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("dayOne").innerHTML = days[d.getDay()];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("dayTwo").innerHTML = days[d.getDay() + 1 ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("dayTwo").innerHTML = days[d.getDay() + 1 ];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("dayThree").innerHTML = days[d.getDay() + 2 ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("dayThree").innerHTML = days[d.getDay() + 2 ];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("dayFour").innerHTML = days[d.getDay() + 3 ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("dayFour").innerHTML = days[d.getDay() + 3 ];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("dayFive").innerHTML = days[d.getDay() + 4 ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("dayFive").innerHTML = days[d.getDay() + 4 ];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("daySix").innerHTML = days[d.getDay() - 2 ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("daySix").innerHTML = days[d.getDay() - 2 ];
 
-var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-document.getElementById("daySeven").innerHTML = days[d.getDay() - 1  ];
+// var days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+// document.getElementById("daySeven").innerHTML = days[d.getDay() - 1  ];
+
+
+$(document).ready(function(){
+ 
+				$('.lightbox').click(function(){
+					$('.backdrop, .box').animate({'opacity':'.5'}, 100, 'linear');
+					$('.box').animate({'opacity':'1.00'}, 100, 'linear');
+					$('.backdrop, .box').css('display', 'block');
+				});
+ 
+				$('.close').click(function(){
+					close_box();
+				});
+ 
+				$('.backdrop').click(function(){
+					close_box();
+				});
+ 
+			});
+ 
+			function close_box()
+			{
+				$('.backdrop, .box').animate({'opacity':'0'}, 100, 'linear', function(){
+					$('.backdrop, .box').css('display', 'none');
+				});
+			}
+
